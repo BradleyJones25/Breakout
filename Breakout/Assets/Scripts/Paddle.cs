d945be79819f3f8fc5258b6649b745e2aa21b9d4
BradleyJones25 <bradleyrobertjones25@gmail.com>
2020-06-06 19:07:01 +0100

    Add paddle movement

--- Breakout/Assets/Scripts/Paddle.cs
+++ Breakout/Assets/Scripts/Paddle.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Paddle : MonoBehaviour {
+public class Paddle : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float paddleIntitialY;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+        paddleIntitialY = this.transform.position.y;
+    }
+
+    private void Update()
+    {
+        PaddleMovement();
+    }
+
+    private void PaddleMovement()
+    {
+        float leftClamp = 135;
+        float rightClamp = 410;
+        float mousePositionPixels = Mathf.Clamp(Input.mousePosition.x, leftClamp, rightClamp);
+        float mouseWorldPositionX = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionPixels, 0, 0)).x;
+        this.transform.position = new Vector3(mouseWorldPositionX, paddleIntitialY, 0);
+    }
 }
