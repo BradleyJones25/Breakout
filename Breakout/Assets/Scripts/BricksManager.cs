4064c97762a8b192c149efab9df1b7caa79be01a
BradleyJones25 <bradleyrobertjones25@gmail.com>
2020-06-08 22:03:46 +0100

    Add levels data

--- Breakout/Assets/Scripts/BricksManager.cs
+++ Breakout/Assets/Scripts/BricksManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -24,5 +25,60 @@ public class BricksManager : MonoBehaviour
 
     #endregion
 
+    private int maxRows = 17;
+    private int maxCols = 12;
+
     public Sprite[] Sprites;
+
+    public List<int[,]> LevelsData { get; set; }
+
+    private void Start()
+    {
+        this.LevelsData = this.LoadLevelsData();
+    }
+
+    private List<int[,]> LoadLevelsData()
+    {
+        // Get data from the text file
+        TextAsset text = Resources.Load("levels") as TextAsset;
+        
+        // Split the data into rows
+        string[] rows = text.text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        
+        // Create a temporary variable to store the levels data
+        List<int[,]> levelsData = new List<int[,]>();
+        int[,] currentLevel = new int[maxRows, maxCols];
+        int currentRow = 0;
+
+        // Iterate every row
+        for(int row = 0; row < rows.Length; row++)
+        {
+            string line = rows[row];
+
+            // Check if the line contains a dash '--'
+            if(line.IndexOf("--") == -1)
+            {
+                // Split the line by a comma ',' - this will give a collection of all the bricks in this line
+                string[] bricks = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                // Iterate every column
+                for(int col = 0; col < bricks.Length; col++)
+                {
+                    // Parse the value and assign to current level
+                    currentLevel[currentRow, col] = int.Parse(bricks[col]);
+                }
+
+                currentRow++;
+            }
+            else
+            {
+                // End of current level
+                // Add the maxtrix to the last and continue the loop
+                currentRow = 0;                           // Reset the current row
+                levelsData.Add(currentLevel);             // Add the current level to the whole levels data
+                currentLevel = new int[maxRows, maxCols]; // Reset the current level to be empty
+            }
+        }
+
+        return levelsData;
+    }
 }
