3280dabd151bc050690896c5aca86c7a88ba3bcc
BradleyJones25 <bradleyrobertjones25@gmail.com>
2020-06-08 16:10:11 +0100

    Add brick and brick destroy effect

---
+++ Breakout/Assets/Scripts/Brick.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class Brick : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    
+    public int HitPoints = 1;
+    public ParticleSystem DestroyEffect;
+
+    public static event Action<Brick> OnBrickDestruction;
+
+    private void Start()
+    {
+        this.sr = this.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+        ApplyCollisionLogic(ball);
+    }
+
+    private void ApplyCollisionLogic(Ball ball)
+    {
+        this.HitPoints--;
+
+        if(this.HitPoints <= 0)
+        {
+            OnBrickDestruction?.Invoke(this);
+            SpawnDestroyEffect();
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            // Change the sprite
+        }
+    }
+
+    private void SpawnDestroyEffect()
+    {
+        Vector3 brickPosition = gameObject.transform.position;
+        Vector3 spawnPosition = new Vector3(brickPosition.x, brickPosition.y, brickPosition.z - 0.2f);
+        GameObject effect = Instantiate(DestroyEffect.gameObject, spawnPosition, Quaternion.identity);
+
+        MainModule mm = effect.GetComponent<ParticleSystem>().main;
+        mm.startColor = this.sr.color;
+        Destroy(effect, DestroyEffect.main.startLifetime.constant);
+    }
+}
